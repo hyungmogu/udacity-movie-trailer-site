90b7f6047d7b622a1229aa591d8d1505b30bffcd
Hyungmo Gu <hyungmogu@users.noreply.github.com>
2017-01-11 14:56:04 -0800

    Add Comments to

--- media.py
+++ media.py
@@ -1,12 +1,12 @@
-# import webbrowser
-# if __name__ == "__main__":
-#     print("Notice: Running main.py in the __main__")
+"""
+What is media.py for?
 
-# else:
-#     print("Notice: Successfully Imported " + __name__ + ".py")
+It houses all data-structures used by the program. The data-structures must be relevant to media.
 
+"""
 class Video():
 
+    
     def __init__(self,title,storyline,poster_image_url,trailer_youtube_url,duration):
         self.title               = title
         self.storyline           = storyline
@@ -16,14 +16,19 @@ class Video():
 
 class Movie(Video):
     """
-    This class provides a way to store information about movies
+    Movie(Video)
+        
+    This data-structure stores information about movies. It is a child of Video Class.
 
-    - title *
-    - storyline *
-    - poster_image *
-    - youtube_trailer *
-    - duration *
+    @allowed_attributes:
+        - title *
+        - storyline *
+        - poster_image *
+        - youtube_trailer *
+        - duration *
 
+    * - inherited attributes
+    ** - non-inheritied attributes
     """
 
     def __init__(self,title,storyline,poster_image,youtube_trailer,duration):
@@ -32,14 +37,23 @@ class Movie(Video):
 
 class TvShow(Video):
     """
+    TvShow(Video)
+        
+    This data-structure stores information about TvShows. It is a child of Video Class.
+
+    @allowed_attributes:    
         - title *
         - storyline *
-        - season
-        - episode
-        - tv_station
+        - season **
+        - episode **
+        - tv_station **
         - poster_image *
         - youtube_trailer *
         - duration *
+   
+    * - inherited attributes
+    ** - non-inheritied attributes
+
     """
     def __init__(self,title,storyline,season,episode,tv_station,poster_image,youtube_trailer,duration):
         Video.__init__(self,title,storyline,poster_image,youtube_trailer,duration)
